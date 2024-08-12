648ce86f00198cb0a66eb32f377a8111955d64eb
isu-nice <isu010614@naver.com>
2024-08-12 15:29:25 +0900

    chore: swagger 어노테이션 추가

--- src/main/java/wanted/pre_onboarding/application/controller/ApplicationController.java
+++ src/main/java/wanted/pre_onboarding/application/controller/ApplicationController.java
@@ -1,5 +1,7 @@
 package wanted.pre_onboarding.application.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import wanted.pre_onboarding.application.dto.ApplicationDto;
 import wanted.pre_onboarding.application.service.ApplicationService;
 
+@Tag(name = "Application", description = "지원 관련 API")
 @RestController
-@RequestMapping("/apply")
+@RequestMapping("/wanted/apply")
 @AllArgsConstructor
 public class ApplicationController {
 
     private final ApplicationService applicationService;
 
+    @Operation(summary = "채용공고에 지원")
     @PostMapping
     public ResponseEntity applyJob(@RequestBody ApplicationDto request) {
         applicationService.applyToJobPosting(request);
