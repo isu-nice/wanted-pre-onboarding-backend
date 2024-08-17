06cab683fb6a7677cde7f60f3ddb5a3f82c6d2d1
isu-nice <isu010614@naver.com>
2024-08-17 23:56:40 +0900

    chore: 어노테이션 추가

--- src/main/java/wanted/pre_onboarding/jobposting/dto/JobPostingDetailsDto.java
+++ src/main/java/wanted/pre_onboarding/jobposting/dto/JobPostingDetailsDto.java
@@ -1,5 +1,6 @@
 package wanted.pre_onboarding.jobposting.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class JobPostingDetailsDto {
     private Long jobPostingId;
     private String companyName;
